14d278a2c6ba6b69d9b82e6f6a8a96ec07467ee1
intelibestluck920 <129634375+intelibestluck920@users.noreply.github.com>
2024-02-19 21:55:41 +0900

    Update twilioController.js

--- server/controllers/twilioController.js
+++ server/controllers/twilioController.js
@@ -2,9 +2,9 @@ const twilio = require('twilio');
 require('dotenv').config();
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER } = process.env;
 
-// const TWILIO_ACCOUNT_SID = "AC1357443a04001311528ef00b80459576";
-// const TWILIO_AUTH_TOKEN = "e01cd83e89b95afbedc2d85c0ad157ad";
-// const TWILIO_PHONE_NUMBER = "17809825081";
+// const TWILIO_ACCOUNT_SID = "****************";
+// const TWILIO_AUTH_TOKEN = "*******************";
+// const TWILIO_PHONE_NUMBER = "******************";
 
 // console.log("hello", TWILIO_ACCOUNT_SID);
 
@@ -21,7 +21,7 @@ exports.sendSMS = async (req, res) => {
         const message = await client.messages.create({
             body: 'Sent SMS',
             from: TWILIO_PHONE_NUMBER,
-            to: '+17809825081',
+            to: '********************',
         });
         console.log('SMS sent successfully: ', message.sid);
         res.status(200).json({ message: 'SMS sent successfully', messageId: message.sid });
@@ -42,7 +42,7 @@ exports.sendSMSWithData = async (req, res) => {
         const message = await client.messages.create({
             body: `${id}'s temperature is ${temperature}`,
             from: TWILIO_PHONE_NUMBER,
-            to: '+17809825081',
+            to: '******************',
         });
         console.log('SMS sent successfully: ', message.body);
         res.status(200).json({ message: 'SMS sent successfully', messageId: message.sid });
